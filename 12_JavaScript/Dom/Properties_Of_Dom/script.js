4d86aa3f5dc2fe60a60a9d953459a6c846752d64
Md-Asif-Sheikh <154016564+Sheikh-ASif@users.noreply.github.com>
2024-08-08 18:51:59 +0530

    Delete 12_JavaScript/Dom directory

--- 12_JavaScript/Dom/Properties_Of_Dom/script.js
+++
@@ -1,7 +0,0 @@
-let div = document.querySelector("div");
-console.dir(div);
-
-///          we can see this in the console
-// to know the ineer text of the html we simple write this div.innerText; to know the text of the div in ths its gives only the text 
-// to know the ineer text of the html and tags of the html we simple write this  div.innerHTML; to know the text of the div div.innerHTML
-//to change the text of an html we simply div.innerText = "New Fruits" do this.
